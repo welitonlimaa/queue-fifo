d4a93d320253bb221d920d540763433fddaef16e
welitonlimaa <welitonlima159@gmail.com>
2023-06-13 20:23:39 -0300

    req 8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -18,4 +18,22 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    search_result = list()
+    for i in range(0, (instance.__len__())):
+        file = instance.search(i)
+        occurrence = list()
+        for j in range(0, file["qtd_linhas"]):
+            line = file["linhas_do_arquivo"][j]
+            if word.lower() in line.lower():
+                occurrence.append({
+                    "linha": (j + 1),
+                    "conteudo": line
+                })
+        if len(occurrence) != 0:
+            result = {
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": occurrence
+            }
+            search_result.append(result)
+    return search_result
