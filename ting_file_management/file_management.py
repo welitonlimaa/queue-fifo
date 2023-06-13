0a7636a5ea4ec99efb57befa8e32c0e0df81e0bc
welitonlimaa <welitonlima159@gmail.com>
2023-06-13 16:40:56 -0300

    req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import os
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not os.path.exists(path_file):
+        mensagem = f"Arquivo {path_file} não encontrado"
+        return print(mensagem, file=sys.stderr)
+    file_name, extension = os.path.splitext(path_file)
+    if extension != '.txt':
+        mensagem = "Formato inválido"
+        return print(mensagem, file=sys.stderr)
+    with open(path_file, 'r') as file:
+        text = file.read()
+    list_lines = text.split("\n")
+    return list_lines
